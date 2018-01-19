cb3471955a65954a4b91dbc175427f98d7350602
yujun666 <yujun666@github.com>
2018-01-19 09:42:08 +0800

    1

--- js/functions.js
+++ js/functions.js
@@ -94,19 +94,19 @@ function startHeartAnimation() {
 
 function timeElapse(date){
 	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date));
-	var days = Math.floor(seconds / (3600 * 24 * 1000));
-	seconds = seconds % (3600 * 24 * 1000);
-	var hours = Math.floor(seconds / (60 * 60 * 1000));
+	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	var days = Math.floor(seconds / (3600 * 24));
+	seconds = seconds % (3600 * 24);
+	var hours = Math.floor(seconds / 3600);
 	if (hours < 10) {
 		hours = "0" + hours;
 	}
-	seconds = seconds % (60 * 60 * 1000);
-	var minutes = Math.floor(seconds / (60 * 1000));
+	seconds = seconds % 3600;
+	var minutes = Math.floor(seconds / 60);
 	if (minutes < 10) {
 		minutes = "0" + minutes;
 	}
-	seconds = seconds % (60 * 1000);
+	seconds = seconds % 60;
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
